d6fb0252faa2753334c8037e48f64d7f2158df95
jP <silva.joao@icen.ufpa.br>
2025-05-26 20:53:25 -0300

    colocando a função CPF em loop

--- src/CPF/Main.java
+++ src/CPF/Main.java
@@ -6,29 +6,32 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int opcao = 1;
+        while (opcao == 1) {
+            teste();
+            System.out.println("Digitar outro CPF? \n 1.Sim \n 2.Não ");
+            opcao = input.nextInt();
+        }
+
+    }
+    static void teste(){
         VerificarCPF cpf = new VerificarCPF();
         Scanner input = new Scanner(System.in);
 
-//        int opcao = 1;
         boolean teste;
         String CPF ;
-//        while (opcao == 1) {
-            System.out.print("Digite um CPF para ser verificado: ");
-            CPF = input.nextLine();
-            teste = cpf.verify(CPF);
 
-            if (teste) {
-                System.out.println("O CPF informado é valido!");
-            } else {
-                System.out.println("O CPF é invalido!");
-            }
 
-//            System.out.print("Digitar outro CPF? \n 1.Sim \n 2.Não ");
-//            opcao = input.nextInt();
-//            if (opcao == 2) {
-//                break;
-//            }
-//        }
+        System.out.print("Digite um CPF para ser verificado: ");
+        CPF = input.nextLine();
+        teste = cpf.verify(CPF);
 
+        if (teste) {
+            System.out.println("O CPF informado é valido!");
+        } else {
+            System.out.println("O CPF é invalido!");
+        }
     }
 }
